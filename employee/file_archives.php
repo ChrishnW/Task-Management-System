3b5acc13d89dec2da88d312a76b73dd04ac80d0e
ChrishnSK <c.lopez@glorylocal.com.ph>
2024-05-10 15:21:24 +0800

    fix bug remove and replace file

--- employee/file_archives.php
+++ employee/file_archives.php
@@ -49,17 +49,17 @@
 											</tr>
 										</thead>
 										<tbody id="tbody">
-                      <?php $query = mysqli_query($con, "SELECT * FROM tasks_details JOIN accounts ON tasks_details.in_charge=accounts.username JOIN task_class on task_class.id=tasks_details.task_class WHERE tasks_details.status='FINISHED' AND tasks_details.requirement_status=1 AND tasks_details.attachment!='' AND tasks_details.task_for='$sec_id' AND tasks_details.approval_status=0");
+                      <?php $query = mysqli_query($con, "SELECT * FROM tasks_details JOIN accounts ON tasks_details.in_charge=accounts.username JOIN task_class on task_class.id=tasks_details.task_class WHERE tasks_details.status='FINISHED' AND tasks_details.requirement_status=1 AND tasks_details.task_for='$sec_id'");
                       if (mysqli_num_rows($query) > 0){
                         while($row=$query->fetch_assoc()){
-                          $date_accomplished = date('d-m-Y h:i A', strtotime($row['date_accomplished']));?>
+                          $date_accomplished = date('Y-m-d h:i A', strtotime($row['date_accomplished']));?>
                         <tr>
                           <td><?php echo $row['task_code'];?></td>
                           <td id='normalwrap'><?php echo $row['task_name'];?></td>
                           <td id='normalwrap'><?php echo $row['attachment'];?></td>
                           <td><?php echo $date_accomplished;?></td>
                           <td><?php echo $row['in_charge']; ?></td>
-                          <td><button type="button" class="btn btn-info" value="<?php echo $row['task_code']?>" data-link="<?php echo $row['attachment'];?>" onclick="viewPDF(this)"><i class="fas fa-book-reader fa-fw"></i> View</button></td>
+                          <td><button type="button" class="btn btn-info" value="<?php echo $row['task_code'];?>" data-link="<?php echo $row['attachment'];?>" data-name="<?php echo $row['task_name'];?>" onclick="viewPDF(this)"><i class="fas fa-book-reader fa-fw"></i> View</button></td>
                         </tr>
                         <?php }
                       }
@@ -76,7 +76,7 @@
 	</div>
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content panel-success">
 			<div class="modal-header panel-heading">
         <a href="#" onclick="goBackAndReload()"><button type="button" class="close">&times;</button></a>
@@ -84,6 +84,7 @@
       </div>
       <div class="modal-body panel-body" id="pdfModal">
         <center><h3 id='documentIDText'></h3></center>
+				<hr>
         <input type="text" id='documentID' hidden>
         <iframe frameborder="0" width="100%" height="400" id="documentPDF"></iframe>
       </div>
@@ -96,11 +97,13 @@
 <script>
 	function viewPDF(element) {
 		var documentID = element.value;
+		var documentName = element.getAttribute("data-name");
     var documentPDF = element.getAttribute("data-link");
 		$(document).ready(function() {
 			$('#exampleModal').modal('show');
 			document.getElementById('documentID').value = documentID;
-      document.getElementById('documentIDText').innerHTML = documentID;
+      // document.getElementById('documentIDText').innerHTML = documentID;
+			document.getElementById('documentIDText').innerHTML = documentName;
       document.getElementById('documentPDF').src = '../documents/Task-Attachments/' + documentPDF;
       document.getElementById('pdfModal').style.display = 'block';
 		});
--- employee/task_details_replace_attachment.php
+++ employee/task_details_replace_attachment.php
@@ -8,37 +8,32 @@
   $con->next_result();
   $targetDir = "../documents/Task-Attachments/";
 
-  $fileName = basename($_FILES["file"]["name"]);
-  $extension = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
-  $fileName = "[" . $username . "_" . $ID . "] " . $fileName;
+  $fileName       = basename($_FILES["file"]["name"]);
+  $extension      = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
+  $fileName       = "[" . $username . "_" . $ID . "] " . $fileName;
   $targetFilePath = $targetDir . $fileName;
-  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+  $fileType       = pathinfo($targetFilePath, PATHINFO_EXTENSION);
 
-  $allowTypes = array('pdf', 'xls', 'xlsx', 'docx', 'pptx', 'txt');
-  if (in_array($fileType, $allowTypes)) {
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
-      $select = "SELECT attachment FROM tasks_details WHERE task_code = '$ID'";
-      $select_result = mysqli_query($con, $select);
-      $row = mysqli_fetch_assoc($select_result);
-      $oldFileName = $row['attachment'];
-
-      $insert = "UPDATE tasks_details SET attachment = '$fileName' WHERE task_code = '$ID'";
-      $insert_result = mysqli_query($con, $insert);
-      if($insert) {
-        if($oldFileName != "" && file_exists($targetDir . $oldFileName)){
-          unlink($targetDir . $oldFileName);
-        }
-        $con->next_result();
-        $systemlog = "INSERT INTO system_log (action, date_created, user) VALUES ('Replace attachment for task [$ID].', '$systemtime', '$username')";
-        $result = mysqli_query($con, $systemlog);
-        echo "Success";
-      }
-    } 
-    else {
-      echo "Unexpected error";
+  $remove         = "UPDATE tasks_details SET attachment=NULL WHERE task_code='$ID'";
+  $remove_result  = mysqli_query($con, $remove);
+  if($remove){
+    if($oldFileName != "" && file_exists($targetDir . $oldFileName)){
+        unlink($targetDir . $oldFileName);
     }
-  } 
-  else {
-    echo "File not supported";
+    else {
+      $allowTypes = array('pdf', 'xls', 'xlsx', 'docx', 'pptx', 'txt');
+      if (in_array($fileType, $allowTypes)) {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+          $insert = "UPDATE tasks_details SET attachment = '$fileName' WHERE task_code = '$ID'";
+          $insert_result = mysqli_query($con, $insert);
+          echo "Success";
+          if($insert_result){
+            $con->next_result(); 
+            $systemlog = "INSERT INTO system_log (action, date_created, user) VALUES ('Replaced file for task [$ID].', '$systemtime', '$username')";
+            $result = mysqli_query($con, $systemlog);
+          }
+        }
+      }
+    }
   }
 ?>
