7a8587a36668887093c6caf155ffaef7d3d9ac18
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-08-30 14:24:47 +0800

    "Updated icon in task card from fa-paperclip to fa-list, and added new card for Report Files with fa-paperclip icon."

--- pages/index.php
+++ pages/index.php
@@ -354,11 +354,30 @@ include('../include/header.php');
                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $assigned_task ?></div>
               </div>
               <div class="col-auto">
-                <i class="fas fa-paperclip fa-3x text-gray-500"></i>
+                <i class="fas fa-list fa-3x text-gray-500"></i>
               </div>
             </div>
             <div class="row mt-3">
               <div class="col"><a href="assign_tasks.php" class="btn btn-info btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Report Files</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $assigned_task ?></div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-paperclip fa-3x text-gray-500"></i>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col"><a href="assign_tasks.php" class="btn btn-success btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
           </div>
