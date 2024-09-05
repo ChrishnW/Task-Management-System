762e9c3602fb209f544a422aebcfa319d093a7e3
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-09-05 13:05:45 +0800

    "Updated footer copyright text"

--- include/footer.php
+++ include/footer.php
@@ -5,7 +5,7 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
-      <span>Copyright &copy; ICT-Information System</span>
+      <span>Developed by ICT - Information System</span>
     </div>
   </div>
 </footer>
