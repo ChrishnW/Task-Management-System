eaf00ac040755892ae76f6c6dcf8afabace80168
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-09-10 10:18:21 +0800

    "Updated 404.php page layout and design for access levels 1, 2, and 3."

---
+++ pages/401.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Task Management System</title>
+
+  <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/Logo.png">
+
+  <!-- Custom fonts for this template -->
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this page -->
+  <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../vendor/snapappointments/bootstrap-select/dist/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
+
+  <!-- Custome scripts for this page -->
+  <script src="../assets/js/moment.min.js"></script>
+</head>
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-lg-4">
+      <div class="text-center mt-4">
+        <img class="img-fluid" src="../assets/img/illustrations/401-error-unauthorized.svg" alt="">
+        <p class="lead">Session timed out. Please log in again.</p>
+        <a class="text-arrow-icon" href="../index.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left ms-0 me-1">
+            <line x1="19" y1="12" x2="5" y2="12"></line>
+            <polyline points="12 19 5 12 12 5"></polyline>
+          </svg>
+          Return to Login
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('../include/footer.php'); ?>
--- pages/404.php
+++ pages/404.php
@@ -1,23 +1,32 @@
-<?php 
-  include('../include/header.php');
+<?php
+include('../include/header.php');
 ?>
 
 <div class="container-fluid">
-  <?php if($access == 1) { ?>
+  <?php if ($access == 1) { ?>
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <div class="text-center mt-4">
+          <img class="img-fluid p-4" src="../assets/img/illustrations/401-error-unauthorized.svg" alt="">
+          <p class="lead">Access to this resource is denied.</p>
+          <a class="text-arrow-icon" href="dashboard-1.html">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left ms-0 me-1">
+              <line x1="19" y1="12" x2="5" y2="12"></line>
+              <polyline points="12 19 5 12 12 5"></polyline>
+            </svg>
+            Return to Dashboard
+          </a>
+        </div>
+      </div>
+    </div>
+  <?php } elseif ($access == 2) { ?>
     <div class="text-center">
       <div class="error mx-auto" data-text="404">404</div>
       <p class="lead text-gray-800 mb-5">Page Not Found</p>
       <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
       <a href="index.php">&larr; Back to Dashboard</a>
     </div>
-  <?php } elseif($access == 2) { ?>
-    <div class="text-center">
-      <div class="error mx-auto" data-text="404">404</div>
-      <p class="lead text-gray-800 mb-5">Page Not Found</p>
-      <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-      <a href="index.php">&larr; Back to Dashboard</a>
-    </div>
-  <?php } elseif($access == 3) { ?>
+  <?php } elseif ($access == 3) { ?>
     <div class="text-center">
       <div class="error mx-auto" data-text="404">404</div>
       <p class="lead text-gray-800 mb-5">Page Not Found</p>
