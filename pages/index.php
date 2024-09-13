257f0e98ec31d5934d310245e514809b74e47d62
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-09-13 13:05:55 +0800

    "Updated dashboard cards with new data and styling"

--- pages/index.php
+++ pages/index.php
@@ -635,30 +635,11 @@ include('../include/header.php');
     <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
     <div class="row">
       <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-          <div class="card-body">
-            <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Current Members</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $members ?></div>
-              </div>
-              <div class="col-auto">
-                <i class="fas fa-users fa-3x text-gray-500"></i>
-              </div>
-            </div>
-            <div class="row mt-3">
-              <div class="col"><a href="accounts.php" class="btn btn-success btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-danger shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Review Tasks</div>
+                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Request for Review Tasks</div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $for_review_tasks ?></div>
               </div>
               <div class="col-auto">
@@ -673,11 +654,30 @@ include('../include/header.php');
         </div>
       </div>
       <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-secondary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Request for Reschedule Tasks</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $for_review_tasks ?></div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-calendar-day fa-3x text-gray-500"></i>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col"><a href="for_review.php" class="btn btn-secondary btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-primary shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Deployed Tasks</div>
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">List of Deployed Tasks</div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $total_tasks ?></div>
               </div>
               <div class="col-auto">
@@ -696,7 +696,7 @@ include('../include/header.php');
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Projects</div>
+                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Current Projects</div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $project_tasks ?></div>
               </div>
               <div class="col-auto">
@@ -706,6 +706,48 @@ include('../include/header.php');
             <div class="row mt-3">
               <div class="col">
                 <a href="404.php" class="btn btn-warning btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Department Members</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $members ?></div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-users fa-3x text-gray-500"></i>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col"><a href="accounts.php" class="btn btn-success btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Department Accomplishment</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $project_tasks ?> out of 100</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-chart-line fa-3x text-gray-500"></i>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col">
+                <a href="404.php" class="btn btn-info btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
           </div>
