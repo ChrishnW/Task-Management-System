0edb0d224986f66894a4367bdae8eda88a370b9d
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-09-14 15:02:29 +0800

    "Swap 'Projects' and 'Assigned Tasks' card content and styles in index.php"

--- pages/index.php
+++ pages/index.php
@@ -333,6 +333,25 @@ include('../include/header.php');
         </div>
       </div>
       <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Assigned Tasks</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $assigned_task ?></div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-list fa-3x text-gray-500"></i>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col"><a href="assign_tasks.php" class="btn btn-info btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-danger shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
@@ -353,25 +372,6 @@ include('../include/header.php');
         </div>
       </div>
       <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-          <div class="card-body">
-            <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Assigned Tasks</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $assigned_task ?></div>
-              </div>
-              <div class="col-auto">
-                <i class="fas fa-list fa-3x text-gray-500"></i>
-              </div>
-            </div>
-            <div class="row mt-3">
-              <div class="col"><a href="assign_tasks.php" class="btn btn-info btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-success shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
@@ -384,7 +384,7 @@ include('../include/header.php');
               </div>
             </div>
             <div class="row mt-3">
-              <div class="col"><a href="files.php" class="btn btn-success btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
+              <div class="col"><a href="404.php" class="btn btn-success btn-sm">More Info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
           </div>
