52aa1d477876be30bb6edfee8cea97f40433c736
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-09-30 12:52:44 +0800

    "Updated daily.php to modify task generation query and insert statement, and removed unnecessary query and conditional statement."

--- include/essential/daily.php
+++ include/essential/daily.php
@@ -11,23 +11,23 @@ if (date('N') >= 1 && date('N') <= 5) {
   if ($dayoff_count > 0) {
     $systemAction = "Today's date is set as a day off; no tasks have been generated.";
   } else {
-    $query_tasks  = mysqli_query($con, "SELECT * FROM tasks WHERE task_class=1 ORDER BY task_name ASC");
+    $query_tasks  = mysqli_query($con, "SELECT * FROM tasks t JOIN task_list tl ON t.task_id=tl.task_id WHERE task_class=1 ORDER BY task_name ASC");
     if (mysqli_num_rows($query_tasks) > 0) {
-      $query_tasks  = mysqli_query($con, "SELECT * FROM tasks WHERE task_class=1 ORDER BY task_name ASC");
       while ($row = $query_tasks->fetch_assoc()) {
+        $taskID     = $row['task_id'];
         $taskName   = $row['task_name'];
         $taskClass  = $row['task_class'];
         $taskFor    = $row['task_for'];
         $inCharge   = $row['in_charge'];
         $submission = $row['submission'];
-        $getFile    = $row['requirement_status'];
-        $taskStatus = 'NOT YET STARTED';
+        $getFile    = $row['attachment'];
         $dueDate    = date('Y-m-d 16:00:00');
-        $latestcode = mysqli_fetch_assoc(mysqli_query($con, "SELECT MAX(task_code) AS latest_task_code FROM tasks_details WHERE task_class='$taskClass' AND task_for='$taskFor'"))['latest_task_code'];
+
+        $latestcode = mysqli_fetch_assoc(mysqli_query($con, "SELECT MAX(task_code) AS latest_task_code FROM tasks_details td JOIN tasks t ON t.task_id=td.task_id JOIN task_list tl ON tl.task_id=td.task_id WHERE task_class='$taskClass' AND task_for='$taskFor'"))['latest_task_code'];
         $numeric_portion = intval(substr($latestcode, -6)) + 1;
         $taskCode = $taskFor . '-TD-' . str_pad($numeric_portion, 6, '0', STR_PAD_LEFT);
 
-        $insert_task = "INSERT INTO tasks_details (`task_code`, `task_name`, `task_class`, `task_for`, `in_charge`, `status`, `date_created`, `due_date`, `requirement_status`, `task_status`) VALUES ('$taskCode', '$taskName', '$taskClass', '$taskFor', '$inCharge', '$taskStatus', '$date_today', '$dueDate', '$getFile', 1)";
+        $insert_task = "INSERT INTO tasks_details (`task_id`, `task_code`, `progress`, `due_date`, `task_status`) VALUES ('$taskID', '$taskCode', 'NOT YET STARTED', '$dueDate', 1)";
         $result_task = mysqli_query($con, $insert_task);
         $taskCount += 1;
       }
@@ -36,8 +36,6 @@ if (date('N') >= 1 && date('N') <= 5) {
       } else {
         $systemAction = "Failed to generate tasks.";
       }
-    } else {
-      $systemAction = 'No daily routine tasks assigned to members found in the system. No tasks have been distributed.';
     }
   }
 } elseif (date('N') == 6 || date('N') == 7) {
