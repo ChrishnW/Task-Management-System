f47c785d1f886e17a6f1d4cc339eabd487d7ea51
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-11-04 14:42:25 +0800

    "Removed conditional check for access level, simplified redirect logic"

--- include/home.php
+++ include/home.php
@@ -3,12 +3,8 @@ include('auth.php');
 
 $con->next_result();
 $data = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM accounts INNER JOIN access ON access.id=accounts.access WHERE username = '$username'"));
-if ($data['access'] !== 1) {
-  if ($data['card'] === NULL || $data['fname'] === NULL || $data['lname'] === NULL || $data['email'] === NULL) {
-    header("location: setup.php");
-  } else {
-    header("location: " . $data['link'] . "");
-  }
+if ($data['card'] === NULL || $data['fname'] === NULL || $data['lname'] === NULL || $data['email'] === NULL) {
+  header("location: setup.php");
 } else {
   header("location: " . $data['link'] . "");
 }
