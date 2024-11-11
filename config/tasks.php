54112d024fca63b458b93abf9b86b041cd9cb08e
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-11-11 12:53:21 +0800

    "Added 'text-truncate' class to task code table cell"

--- config/tasks.php
+++ config/tasks.php
@@ -84,7 +84,7 @@ if (isset($_POST['filterTableTask'])) {
             echo '<input type="checkbox" name="selected_ids[]" class="form-control bodyCheckbox" disabled>';
           } ?>
         </td>
-        <td><?php echo $row['task_code'] ?></td>
+        <td class="text-truncate"><?php echo $row['task_code'] ?></td>
         <td>
           <?php echo $row['task_name'] ?>
           <i class="fas fa-info-circle" data-toggle="tooltip" data-placement="right" title="<?php echo $row['task_details'] ?>"></i>
